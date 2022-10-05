94242a4ee1b31fd4921478209536a695a9ddc0d2
payalasodariya <payalasodariya30@gmail.com>
2022-10-05 16:35:44 -0400

    Init: Created react structure and portfolio page

---
+++ containers/Projects.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { projects } from "../portfolio";
+import { Container, Row } from "reactstrap";
+import ProjectsCard from "../components/ProjectsCard";
+import { Fade } from "react-reveal";
+
+const Projects = () => {
+	return (<></>);
+};
+
+export default Projects;
---
+++ portfolio.js
@@ -0,0 +1,203 @@
+import emoji from "react-easy-emoji";
+
+export const greetings = {
+  name: "Payal Baldha",
+  title: "Hi all, I'm Payal",
+  description:
+    "I'm passionate Frontend Developer having an experience of 4+ years. I have experience working with Angular, VueJs, ReactJs and NodeJs.",
+  resumeLink:
+    "https://docs.google.com/document/d/1lwE4_TYljbuAfxTqgEgCbEqBjjNCpROpjEk1Xz0sLqU/edit?usp=sharing",
+};
+
+export const openSource = {
+  githubUserName: "payalasodariya",
+};
+
+export const contact = {};
+
+export const socialLinks = {
+  // instagram: "https://www.instagram.com/Baldha___",
+  twitter: "https://twitter.com/AsodariyaPayal",
+  github: "https://github.com/payalasodariya",
+  linkedin: "https://www.linkedin.com/in/payalasodariya",
+};
+
+export const skillsSection = {
+  title: "What I do",
+  subTitle: "Frontend Development | Backend Development ",
+  data: [
+    {
+      title: "Frontend Development & Backend Development",
+      lottieAnimationFile: "/lottie/skills/fullstack.json", // Path of Lottie Animation JSON File
+      skills: [
+        emoji("⚡ Building Reusable Component Based Web Applications"),
+        emoji("⚡ Building RESTful APIs"),
+      ],
+      softwareSkills: [
+        {
+          skillName: "Angular",
+          fontAwesomeClassname: "vscode-icons:file-type-angular",
+        },
+        {
+          skillName: "Nodejs",
+          fontAwesomeClassname: "logos:nodejs",
+        },
+        {
+          skillName: "ReactJs",
+          fontAwesomeClassname: "vscode-icons:file-type-reactts",
+        },
+        {
+          skillName: "ViewJs",
+          fontAwesomeClassname: "vscode-icons:file-type-vue",
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "logos:javascript",
+        },
+        
+        {
+          skillName: "NPM",
+          fontAwesomeClassname: "logos:npm-icon",
+        },
+        {
+          skillName: "Yarn",
+          fontAwesomeClassname: "logos:yarn",
+        },
+        {
+          skillName: "HTML-5",
+          fontAwesomeClassname: "vscode-icons:file-type-html",
+        },
+        {
+          skillName: "CSS-3",
+          fontAwesomeClassname: "vscode-icons:file-type-css",
+        },
+      ],
+    },
+    // ,
+    // {
+    // 	title: "Blockchain",
+    // 	lottieAnimationFile: "/lottie/skills/ethereum.json", // Path of Lottie Animation JSON File
+    // 	skills: [
+    // 		emoji(
+    // 			"⚡ Experience in developing Smart Contract using Solidity & Ethereum"
+    // 		),
+    // 		emoji(
+    // 			"⚡ Building Scripts for automated testing & deployment of Smart Contracts using Brownie & Infura"
+    // 		),
+    // 		emoji(
+    // 			"⚡ Experience of using Openzeppelin Smart Contract Standards & Chainlink Oracles"
+    // 		),
+    // 		emoji(
+    // 			"⚡ Developing NFT Smart Contracts using ERC-721 Token Standard"
+    // 		),
+    // 		emoji(
+    // 			"⚡ Building Dapps with React.js & Solidity using Web3.js, Moralis & IPFS"
+    // 		),
+    // 	],
+    // 	softwareSkills: [
+    // 		{
+    // 			skillName: "Ethereum",
+    // 			fontAwesomeClassname: "logos:ethereum",
+    // 		},
+    // 		{
+    // 			skillName: "Solidity",
+    // 			fontAwesomeClassname: "logos:solidity",
+    // 		},
+    // 		{
+    // 			skillName: "Web3js",
+    // 			fontAwesomeClassname: "logos:web3js",
+    // 		},
+    // 		{
+    // 			skillName: "Metamask",
+    // 			fontAwesomeClassname: "logos:metamask-icon",
+    // 		},
+    // 		{
+    // 			skillName: "Ganache",
+    // 			fontAwesomeClassname: "logos:ganache-icon",
+    // 		},
+    // 	],
+    // },
+  ],
+};
+
+export const SkillBars = [
+  // {
+  // 	Stack: "Frontend/Design", //Insert stack or technology you have experience in
+  // 	progressPercentage: "90", //Insert relative proficiency in percentage
+  // },
+  // {
+  // 	Stack: "Backend",
+  // 	progressPercentage: "70",
+  // },
+  // {
+  // 	Stack: "Programming",
+  // 	progressPercentage: "60",
+  // },
+];
+
+export const educationInfo = [
+  {
+    University: "Gujarat Technological University",
+    subHeader: "Bachelor of Engineering in Computer Engineering",
+    duration: "Aug 2013 - May 2017",
+    desc: "",
+    descBullets: ["Completed my graduation by securing 8.5 CGPA"],
+  },
+];
+
+export const experience = [
+  {
+    role: "Application development senior analyst ",
+    company: "Accenture",
+    companylogo: "/img/icons/common/Accenture-Logo.png",
+    date: "Jan 2022 – July 2022",
+    desc: "I worked as a Senior Software Developer at Accenture.",
+    descBullets: [
+      "Responsible for software development using Angular and maintenance across the codebase",
+      "Migrating codebase and producing maintainable code that respects established standards and best practices",
+      "Contributed to the continuous improvement of the product through agile methodology",
+    ],
+  },
+  {
+    role: "Full stack developer",
+    company: "Codeflash Infotech",
+    companylogo: "/img/icons/common/codeflash.png",
+    date: "Jan 2020 – Dec 2021",
+    desc: "Here being a full stack developer, my role was to work with ReactJs, VueJs and NodeJs.",
+    descBullets: [
+      "Implemented various pages using react js(typescript) reusable components, used redux and redux-saga for state management",
+      "Integrated payment gateway(Stripe) in ecommerce webapp and used google maps, angular instant search, graphQL and firebase for authentication",
+      "Created architecture for web application",
+      "experience in RESTful web services to integrate between client and server side"
+    ],
+  },
+  {
+    role: "Tech Lead",
+    company: "Srkay Consulting Group",
+    companylogo: "/img/icons/common/scikey.png",
+    date: "June 2017 - Jan 2020",
+    desc: "Developed a high-performance web platform for a SaaS product SCIKEY, used by 100+ clients",
+    descBullets: [
+      "Designed few feature flows, UI and databases",
+      "Initiated Angular web application base structures and npm packages for future effort minimization",
+      "Worked as a front end developer(Angular 2, 4, 5), Implemented chat bot(Angular, NodeJs, Google Dialog-flow, Socket Programming, mongodb)",
+      "Generated REST APIs and integrated with the front end",
+    ],
+  },
+];
+
+export const projects = [
+];
+
+export const feedbacks = [
+  // {
+  // 	name: "John Smith",
+  // 	feedback:
+  // 		"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.",
+  // },
+  // {
+  // 	name: "John Smith",
+  // 	feedback:
+  // 		"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, vel illo. Eum magnam beatae ratione eos natus accusamus unde pariatur fugiat at facilis, modi molestiae? Labore odio sit eligendi. Tenetur.",
+  // },
+];
