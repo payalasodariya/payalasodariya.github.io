54df438d06e1e25dc6903b7b461e17fe5f77a51c
payalasodariya <payalasodariya30@gmail.com>
2022-10-07 11:52:42 -0400

    Updates

--- _next/static/FMMXRG_fenSWGeHS8B6qV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0e29bded5e1e16c4c7b7.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
