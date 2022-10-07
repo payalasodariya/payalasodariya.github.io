925997881cbccd1a09d15e66e01f388c9b96cb39
payalasodariya <payalasodariya30@gmail.com>
2022-10-07 11:51:55 -0400

    feat: Add footer email and contact

--- components/GithubProfileCard.jsx
+++ components/GithubProfileCard.jsx
@@ -2,41 +2,63 @@ import React from "react";
 
 import { Card, Col, Row, Container } from "reactstrap";
 
-import SocialLinks from "../components/SocialLinks";
-import Image from "next/image";
-
 const GithubProfileCard = ({ prof }) => {
-	return (
-		<Card className="section-lg bg-gradient-info shadow-lg border-0">
-			<Container className="">
-				<div className="p-2">
-					<Row className="">
-						<Col className="order-lg-2" lg="4">
-							<img
-								src={prof.avatar_url}
-								style={{ width: "200px" }}
-								alt=""
-								className="rounded-circle img-center img-fluid shadow shadow-lg--hover mb-4"
-							/>
-						</Col>
-						<Col lg="8" className="order-lg-1">
-							<h2 className="text-white">Reach Out to me!</h2>
+  return (
+    <Card className="section-lg bg-gradient-info shadow-lg border-0">
+      <Container className="">
+        <div className="p-2">
+          <Col>
+            <img
+              src={prof.avatar_url}
+              style={{ width: "150px" }}
+              alt=""
+              className="rounded-circle img-center img-fluid shadow shadow-lg--hover mb-4"
+            />
+          </Col>
+          <Row>
+            <Col lg="4"></Col>
+            <Col lg="8" className="">
+              <Col className="">
+                <h2 className="text-white">Reach Out to me!</h2>
+                <div class="footer-center">
+                  <div>
+                    <i className="fa fa-map-marker"></i>
+                    <p>
+                      <span>{prof.location}</span>
+                    </p>
+                  </div>
+
+                  <div>
+                    <i className="fa fa-phone"></i>
+                    <p>+1 (613) 602 0476</p>
+                  </div>
+
+                  <div>
+                    <i className="fa fa-envelope"></i>
+                    <p>
+                      <a href="mailto:payalasodariya30@gmail.com">
+                        payalasodariya30@gmail.com
+                      </a>
+                    </p>
+                  </div>
+                </div>
+                {/* <h2 className="text-white">Reach Out to me!</h2>
 							<p className="lead text-white mt-3">
-								DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY
-								INBOX IS OPEN FOR ALL
+							<i className="ni ni-email-83 mr-2" /> payalasodariya30@gmail.com
+							<i className="ni ni-mobile-button mr-2" /> (613)602-0476
 							</p>
 							<p className="text-white mt-3">{prof.bio}</p>
 							<div className="my-3 icon-shape bg-gradient-white shadow rounded text-info">
 								<i className="ni ni-pin-3 text-info mr-2" />
 								{prof.location}
-							</div>
-							<SocialLinks />
-						</Col>
-					</Row>
-				</div>
-			</Container>
-		</Card>
-	);
+							</div> */}
+              </Col>
+            </Col>
+          </Row>
+        </div>
+      </Container>
+    </Card>
+  );
 };
 
 export default GithubProfileCard;
